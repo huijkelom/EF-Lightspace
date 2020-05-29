098ea410097185881f4f55e39c9ecde97bb6ddf9
Bunnybom <77199@rocteraa-student.nl>
2020-05-29 14:53:12 +0200

    Gamefile setup

---
+++ LightSpace_WPF_Engine/LightSpace_WPF_Engine/Games/DeVloerIsLava/FloorIsLava.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using LightSpace_WPF_Engine.Models.Enums;
+using LightSpace_WPF_Engine.Models.Models;
+using LightSpace_WPF_Engine.Models.Utility;
+
+namespace LightSpace_WPF_Engine.Games.VLoerIsLava
+{
+    public class FloorIsLava : RunningGameBehavior
+    {
+
+    }
+}
