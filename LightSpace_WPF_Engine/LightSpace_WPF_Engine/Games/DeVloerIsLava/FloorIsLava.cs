ae1767c3ac0d73f285d626ac20a26e8a5d2859d6
Bunnybom <77199@rocteraa-student.nl>
2020-05-29 15:48:09 +0200

    Startgame Signal

--- LightSpace_WPF_Engine/LightSpace_WPF_Engine/Games/DeVloerIsLava/FloorIsLava.cs
+++ LightSpace_WPF_Engine/LightSpace_WPF_Engine/Games/DeVloerIsLava/FloorIsLava.cs
@@ -17,6 +17,8 @@ namespace LightSpace_WPF_Engine.Games.VLoerIsLava
         private List<Tile> GoodTiles = new List<Tile>();
         private List<Tile> resetTile = new List<Tile>();
         private int goodTilesAmount = 10;
+        private int counterStart = 0;
+        private bool startGame = true;
 
         public override void Start()
         {
@@ -37,9 +39,57 @@ namespace LightSpace_WPF_Engine.Games.VLoerIsLava
 
             goodTilesAmount = GameFieldTileSize.X * GameFieldTileSize.Y / 2;
 
+
             backgroundImage = new Bitmap(gameFieldWidth, gameFieldHeight);
             backgroundImage.DrawRectangle(Vector2.Zero(), gameFieldWidth, gameFieldHeight, true, 2, Colors.Black());
             Game.Get.TileManager.SetRenderGraphic(backgroundImage);
+        }
+
+        // Gives the start signal of the game.
+        public void StartGame(Bitmap tempBG)
+        {
+            var allTiles = Game.Get.TileManager.Tiles;
+            var color = Colors.Red();
+
+            // Goes through 4 different colors to make the startsignal
+            switch (counterStart)
+            {
+                case 0:
+                    color = Colors.Red();
+                    break;
+                case 1:
+                    color = Colors.Black();
+                    break;
+                case 2:
+                    color = Colors.Orange();
+                    break;
+                case 3:
+                    color = Colors.Black();
+                    break;
+                case 4:
+                    color = Colors.Green();
+                    break;
+                case 5:
+                    color = Colors.Black();
+                    break;
+                default:
+                    break;
+            }
+
+            // Draws all tiles one color on the background
+            foreach (var tile in allTiles)
+            {
+                var pos = new Vector2(tile.Position.X * lightAmount, tile.Position.Y * lightAmount);
+
+                tempBG.DrawRectangle(pos, lightAmount - 1, lightAmount - 1, true, 0, color);
+            }
+
+            if (counterStart > 4)
+            {
+                startGame = false;
+            }
+
+            counterStart++;
         }
     }
 }
